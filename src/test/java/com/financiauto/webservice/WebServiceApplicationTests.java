2d1687a346c041022a378085c21ed1b1d98cb74c
jackdelaguilavilla03 <jackdelaguilavilla03>
2023-10-20 21:09:44 -0500

    feat: create proyect

---
+++ src/test/java/com/financiauto/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financiauto.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
