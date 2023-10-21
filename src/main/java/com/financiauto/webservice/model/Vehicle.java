fb49e008c47fedf5a91efb7f01bb794fb8693db3
María Alexandra Ahuanari Murayari <U20201A836@UPC.EDU.PE>
2023-10-21 11:26:11 -0500

    feat(vehicle): added model and repository for vehicle

--- src/main/java/com/financiauto/webservice/model/Vehicle.java
+++ src/main/java/com/financiauto/webservice/model/Vehicle.java
@@ -1,4 +1,21 @@
 package com.financiauto.webservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer value;
 }
--- src/main/java/com/financiauto/webservice/repository/VehicleRepository.java
+++ src/main/java/com/financiauto/webservice/repository/VehicleRepository.java
@@ -1,4 +1,11 @@
 package com.financiauto.webservice.repository;
 
-public class VehicleRepository {
+import com.financiauto.webservice.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+    Optional<Vehicle> findByUserId(Integer userId);
 }
